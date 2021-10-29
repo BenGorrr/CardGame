eac8a736c80dccd8c3e8971fd903046818c6fa3c
BenGorrr <ggbengaben@gmail.com>
2021-10-29 12:10:11 +0800

    minor changes

--- card.py
+++ card.py
@@ -6,7 +6,8 @@ class Card():
         self.suite = suite
         self.face = face
         self.spoints = SUITES.index(self.suite)
-        self.fpoints = FACES.index(self.face) + 1
+        #self.fpoints = FACES.index(self.face) + 1
+        self.fpoints = FACES.index(self.face) + 1 if FACES.index(self.face) < 9 else 10
         self.cardStr = self.suite + self.face
         #self.points = self.toPoints()
 
@@ -14,7 +15,7 @@ class Card():
         return FACES.index(self.face) + 1
 
     def __str__(self):
-        return f"Card: {self.suite + self.face} Points: {self.points}"
+        return f"Card: {self.suite + self.face}"
 
     def __repr__(self):
         return self.suite + self.face
